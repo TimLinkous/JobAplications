28c2d81217f5ebbe9e90ddc3a5d71e2b938e1076
Tim L <timothy.m.linkous@gmail.com>
2024-09-18 15:30:54 -0400

    job app to row function

--- src/google_sheets_manager.py
+++ src/google_sheets_manager.py
@@ -5,6 +5,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from typing import List, Dict
 from job_application import JobApplication
+import ast
 
 class GoogleSheetsManager:
     def __init__(self, credentials_path: str, spreadsheet_id: str):
@@ -41,7 +42,6 @@ class GoogleSheetsManager:
         except HttpError as error:
             print(f"An error occurred: {error}")
             
-
     def clear_sheet(self, range_name: str):
         try:
             self.sheet.values().clear(spreadsheet_ID=self.spreadsheet_id, range=range_name).execute()
@@ -50,10 +50,27 @@ class GoogleSheetsManager:
             print(f"An error occurred: {error}")
 
     def job_application_to_row(self, job: JobApplication) -> List:
-        pass
+        return [
+            job.title, job.company, job.url, job.location, job.job_type,
+            str(job.date_applied), str(job.deadline), job.description, job.status,
+            ', '.join(map(str, job.follow_up_dates)),
+            str(job.last_activity),
+            str(job.contact_info),
+            str(job.interview_info),
+            str(job.salary_info),
+            str(job.documents)
+        ]
 
     def row_to_job_application(self, row: List) -> JobApplication:
-        pass
+        job = JobApplication(*row[:9])  # first 9 elements
+        job.follow_up_dates = [date.fromisoformat(d.strip()) for d in row[9].split(',')] if row[9] else[]
+        job.last_activity = date.fromisoformat(row[10] if row[10] else None)
+        job.contact_info = ast.literal_eval(row[11]) if row[11] else {}
+        job.interview_info = eval(row[12]) if row[12] else {}
+        job.salary_info = eval(row[13]) if row[13] else {}
+        job.documents = eval(row[14]) if row[14] else {}
+        return job
+
 
     def sync_job_applications(self, jobs: List[JobApplication]):
         pass
